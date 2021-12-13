b84d4000881a6b824be33e171f2ad493e3aa3545
Jayabrata <joyj03787@gmail.com>
2021-12-14 04:30:32 +0530

    fixed code error

--- Lectures/Lec2/example_stream.cpp
+++ Lectures/Lec2/example_stream.cpp
@@ -5,19 +5,15 @@ using namespace std;
 
 int main() {
     // Combine variables into a stringstream .
-    stringstream filename{" 00205. txt"};
+    stringstream filename{"00205. txt"};
     // Create variables to split the string stream
     int num = 0;
     string ext;
     // Split the string stream using simple syntax
     filename >> num >> ext;
-}
-// Tell your friends
-cout << " Number
-        is : " << num << endl;
-             cout
-     << " Extension is: "
-     << ext
-     << endl;
-return 0;
+
+    // Tell your friends
+    cout << " Number is : " << num << endl;
+    cout << " Extension is: " << ext << endl;
+    return 0;
 }
