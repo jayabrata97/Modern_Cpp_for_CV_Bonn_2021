8e794f60b5b0703859b0bc44dc4cb0b74e768919
Jayabrata <joyj03787@gmail.com>
2021-12-16 05:26:02 +0530

    using gcc

---
+++ homework/homework_2/task_1/build/CMakeFiles/task_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/task_1"
+  "../bin/task_1.pdb"
+  "CMakeFiles/task_1.dir/task_1.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
