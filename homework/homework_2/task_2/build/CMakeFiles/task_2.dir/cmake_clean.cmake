800990f04a0e4c71b37f4cb64dcef3881bdd3257
Jayabrata <joyj03787@gmail.com>
2021-12-16 05:48:43 +0530

    modified build directory for task_2

---
+++ homework/homework_2/task_2/build/CMakeFiles/task_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/task_2"
+  "../bin/task_2.pdb"
+  "CMakeFiles/task_2.dir/task_2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
