115cc09bb5d40ff0534c0a0c2603b80b49fed406
Jayabrata <joyj03787@gmail.com>
2022-04-24 15:23:33 +0530

    updated

---
+++ Lectures/Lec4/algo_sort.cpp
@@ -0,0 +1,11 @@
+#include <array>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    std::array<int, 10> s = {5, 7, 4, 2, 8, 6, 1, 9, 0, 3};
+
+    cout << "Before sorting: ";
+    cout << s;
+}
---
+++ Lectures/Lec4/iterator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+    std::vector<double> x{1, 2, 3};
+    for (auto it = x.begin(); it != x.end(); ++it) {
+        cout << *it << endl;
+    }
+    // Map iterators
+    std::map<int, string> m = {{1, "hello"}, {2, "world"}};
+    std::map<int, string>::iterator m_it = m.find(1);
+    cout << m_it->first << ":" << m_it->second << endl;
+
+    auto m_it2 = m.find(1);  // same thing
+    cout << m_it2->first << ":" << m_it2->second << endl;
+
+    if (m.find(3) == m.end()) {
+        cout << "Key 3 was not found\n";
+    }
+    return 0;
+}
